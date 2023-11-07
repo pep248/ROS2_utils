30217f3a22eb1aa643aae1597ee1981c3ad3e9c6
pep248 <rueda_999@hotmail.com>
2023-11-07 15:26:52 +0100

    Webcam code fixed and readme updated

--- src/video_2_ros2_topic/video_2_ros2_topic/webcam.py
+++ src/video_2_ros2_topic/video_2_ros2_topic/webcam.py
@@ -4,19 +4,22 @@ from rclpy.node import Node
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import sys
+import os
 
 class ImagePublisher(Node):
-    def __init__(self):
+    def __init__(self,video_device : int, topic : str):
         super().__init__("image_publisher")
         self.bridge = CvBridge()
-        self.cap = cv2.VideoCapture(0)
-        self.pub = self.create_publisher(Image, "/image", 10)
-        self.rgb8pub = self.create_publisher(Image, "/image/rgb", 10)
-        self.bgr8pub = self.create_publisher(Image, "/image/bgr", 10)
-        self.mono8pub = self.create_publisher(Image, "/image/mono", 10)
+        self.cap = cv2.VideoCapture(int(video_device))
+        self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+        self.pub = self.create_publisher(Image, topic, 10)
+        self.rgb8pub = self.create_publisher(Image, topic + "/rgb", 10)
+        self.bgr8pub = self.create_publisher(Image, topic + "/bgr", 10)
+        self.mono8pub = self.create_publisher(Image, topic + "/mono", 10)
 
     def run(self):
-        while True:
+        while rclpy.ok():
             try:
                 r, frame = self.cap.read()
                 if not r:
@@ -36,12 +39,17 @@ class ImagePublisher(Node):
 
             except CvBridgeError as e:
                 print(e)
+       
         self.cap.release()
 
 def main(args=None):
     rclpy.init(args=args)
-
-    ip = ImagePublisher()
+        
+    if(len(sys.argv) != 3):
+        print("Incorrect number of arguments\nUsage:\n\tros2 run video_2_ros2_topic webcam -- <video_device_number> </topic> ")
+        exit()
+    else:
+        ip = ImagePublisher(sys.argv[1], sys.argv[2])
     print("Publishing...")
     ip.run()
 
